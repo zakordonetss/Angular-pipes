a6ec093d7bcb535c6c0e97d16f9ff4a542e64d95
zakordonetss <60739399+zakordonetss@users.noreply.github.com>
2021-01-29 17:14:44 +0200

    added filter pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { FilterPipe } from './pipes/filter.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FilterPipe
   ],
   imports: [
     BrowserModule,

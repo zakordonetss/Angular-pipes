93bca7b324a21bd6b5b9111e2bdfa52cede65d8f
zakordonetss <60739399+zakordonetss@users.noreply.github.com>
2021-01-29 11:07:22 +0200

    added styles and posts array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
+export interface Post {
+    title: string,
+    text: string,
+    id?: number,
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'Angular-pipes';
+    posts: Post[] = [
+        {title: 'Kiev', text: 'Kiev is the capital of Ukrain', id: 1},
+        {title: 'Moscow', text: 'Moscow is the capital of Russia'},
+        {title: 'Minsk', text: 'Minsk is the capital of Belarus'},
+        {title: 'Warsaw', text: 'Warsaw is the capital of Poland'},
+    ]
 }
